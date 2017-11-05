4cb74e53dd9735909d321b198bb496c053342a83
Drew Overcash <drewovercash@gmail.com>
2017-11-05 21:53:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ function viewCart() {
     if (cart.length === 1) {
       string += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`;
     } else if (cart.length === 2) {
-      string += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and
-      ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`;
+      string += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`
     } else {
       console.log(`${string}.`);
     };
