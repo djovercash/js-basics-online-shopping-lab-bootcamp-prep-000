6912d9de921d94c71e225ce0f0a64a6f6dab531c
Drew Overcash <drewovercash@gmail.com>
2017-11-05 20:44:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var itemPrice = Math.floor(Math.random() * 100 + 1);
-  var item = new Object ({ itemName: itemPrice});
+  var item = new Object({ itemName: itemPrice});
   cart.push(item);
   console.log(`${itemName} has been added to your cart.`);
   return cart;
